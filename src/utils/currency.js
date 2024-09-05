5749b42d14184d93d32478a156ae7253211c74a7
Leo <leo.pratama@aqi.co.id>
2024-09-05 16:30:03 +0700

    update: currency format

--- src/utils/currency.js
+++ src/utils/currency.js
@@ -1,6 +1,8 @@
 export function currency(value){
     if(!Number.isFinite(value)){
         return "Rp 0";
+    }else if(value < 0){
+        return "-Rp " + Math.abs(value).toLocaleString('id-ID');
     }
     return "Rp " + value.toLocaleString('id-ID');
 }
